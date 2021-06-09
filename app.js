345edea0a884a50922094eae6ba750bdbd446bec
Dwayne Brown <dwayne@pebr.co.uk>
2021-06-09 13:20:48 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import router from "./router";
 
 (async function () {
+  
   const domain = window.env.AUTH0_DOMAIN;
   const client_id = window.env.CLIENT_ID;
   const redirect_uri = window.env.APP_URL;
